dfccfab9cdf3edde8b802f8fc3bcfc5d873a065b
quocthinh78 <thinhvqpoly@gmail.com>
2024-09-13 20:36:12 +0700

    test 3

--- src/components/chat-header.tsx
+++ src/components/chat-header.tsx
@@ -32,7 +32,8 @@ const ChatHeader: FC<ChatHeaderProp> = ({ title, chatId, userData }) => {
           size={24}
         />
         1
-        2
+        2.3
+        3.1
       </div>
     </div>
   );
